62377768b5b3f6db1905506531f2ebc65b08bfd2
Adson-Macedo <agnsoft@hotmail.com>
2020-09-25 11:56:56 -0300

    Finalizacao da exibicao de dados

--- index.js
+++ index.js
@@ -3,10 +3,27 @@ const express = require('express')
 const app = express()
 
 const storage = {
-    alunosCont: 0,
-    alunos: [],
-    projetosCont: 0,
-    projetos: [],
+    alunosCont: 1,
+    alunos: [
+        {   //  mock
+            id: 1,
+            nome: "Thairam Michel",
+            email: "a@a.a",
+            matricula: "12321312",
+            idade: 25,
+            curso: "Ciencia da Computação"
+        }    
+    ],
+    projetosCont: 1,
+    projetos: [
+        {   //mock
+            id: 1,
+            descricao: "Teste de Software",
+            orientador: "Sabrina Souto",
+            duracao: 5,
+            alunos: []
+        }    
+    ],
 
     gerarIdAluno() {
         return ++this.alunosCont
@@ -34,19 +51,23 @@ const storage = {
     adicionarAlunoAoProjeto(idAluno, idProjeto) {
         const aluno = this.encontrarAlunoPeloId(idAluno)
         const projeto = this.encontrarProjetoPeloId(idProjeto)
-        
-        projeto.alunos.push(aluno)
+
+        if (aluno && projeto && !projeto.alunos.filter(aluno => idAluno === aluno.id).length)
+            projeto.alunos.push(aluno)
         
         return projeto
     },
-    adicionarAlunosAoProjeto(alunos, idProjeto) {
+    removerAlunoDoProjeto(idAluno, idProjeto) {
+        const aluno = this.encontrarAlunoPeloId(idAluno)
         const projeto = this.encontrarProjetoPeloId(idProjeto)
-        alunos.map(idAluno => this.encontrarAlunoPeloId(idAluno))
-                .forEach(aluno => projeto.alunos.push(aluno))
+        
+        aluno && projeto && projeto.alunos.pop(aluno)
+        
         return projeto
     },
     getAlunosDoProjeto(idProjeto) {
         const projeto = this.projetos.filter(pr => pr.id === idProjeto)[0]
+
         return projeto.alunos
     }
 }
@@ -61,11 +82,23 @@ app.get('/alunos', (req, res) =>
 app.get('/alunos/:id', (req, res) => 
     res.send(storage.encontrarAlunoPeloId(req.params.id)))
 
-app.post('/alunos', (req, res) => 
-    res.send(storage.criarAluno(req.body)))
+app.post('/alunos', (req, res) => {
+    const nome = req.body.nome
+
+    if (storage.alunos.filter(aluno => nome === aluno.nome).length){
+        res.sendStatus(409)
+    }else{
+        res.send(storage.criarAluno(req.body))
+    }
+})
 
 app.get('/projetos', (req, res) => {
-    const projetos = [...storage.projetos]
+    const projetos = []
+
+    storage.projetos.forEach(projeto => {
+        const newProj = {...projeto}
+        projetos.push(newProj)
+    })
     
     projetos.map(projeto => {
         delete projeto.alunos
@@ -75,15 +108,18 @@ app.get('/projetos', (req, res) => {
 })
 
 app.get('/projetos/:id', (req, res) => 
-    res.send(storage.encontrarProjetoPeloId(req.params.id)))
+    res.send(storage.encontrarProjetoPeloId(parseInt(req.params.id))))
 
 app.post('/projetos', (req, res) =>
     res.send(storage.criarProjeto(req.body)))
 
 app.get('/projetos/:id/alunos', (req, res) => 
-    res.send(storage.getAlunosDoProjeto(req.params.id)))
+    res.send(storage.getAlunosDoProjeto(parseInt(req.params.id))))
 
 app.post('/projetos/:id/alunos', (req, res) => 
-    res.send(storage.adicionarAlunosAoProjeto(req.body.idAlunos, req.params.id)))
+    res.send(storage.adicionarAlunoAoProjeto(parseInt(req.body.idAluno), parseInt(req.params.id))))
+
+app.delete('/projetos/:id/alunos', (req, res) => 
+    res.send(storage.removerAlunoDoProjeto(parseInt(req.body.idAluno), parseInt(req.params.id))))
 
 app.listen(3000, () => console.log('Servidor Executando...'))
--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const getData = () => {
     return data
 }
 
-const setValidation = (form) => {
+const configureForm = (form) => {
     form && form.addEventListener('submit', event => {
         event.preventDefault()
         event.stopPropagation();
@@ -22,23 +22,27 @@ const setValidation = (form) => {
                 method,
                 url,
                 data: getData(),
-            })
+            }).then( res => {
+                const lista = form.getAttribute('lista')
+                alert('Salvo com Sucesso!')
+                getPage(lista)
+            }).catch(err => alert('Registro duplicado!'))
+
         }
     }, false)
 }
 
 const getPage = (url) => {
-    console.log(url)
     $.get(url, response => {
         $('#destino').html(response);
         const form = $('.needs-validation').get(0)
         const tabela = $('.table').get(0)
 
-        form && setValidation(form)
+        form && configureForm(form)
 
-        if (tabela){
+        if (tabela) {
             loadData(tabela)
-            const title = $('.lista').attr('title')
+            const title = $('.lista').attr('titulo')
             $('.list-title').html(title)
         }
     })
@@ -47,22 +51,26 @@ const getPage = (url) => {
 const toRow = (item, botoes) => {
     const tr = $('<tr>')
     Object.values(item).forEach(value => tr.append(`<td>${value}</td>`))
-    tr.append(botoes)
+    botoes && tr.append(botoes)
 
     return tr
 }
 
 const loadData = (tabela) => {
-    const botoes = tabela.getAttribute('id') === 'tabela-alunos' ? getBotoesAluno : getBotoesProjeto
+    let myButtons
 
     const url = tabela.getAttribute('url')
-    const tBody = $('tbody')
+    const tBody = $(tabela).find('tbody')
     tBody.html('')
 
     $.get(url)
         .then(items => items.map(item => {
-            const myButtons = botoes(item.id)
-            return toRow(item, myButtons) 
+            if (tabela.getAttribute('id') === 'tabela-adicionar-alunos')
+                myButtons = getBotoesAluno(item.id)
+            else if (tabela.getAttribute('id') === 'tabela-projetos')
+                myButtons = getBotoesProjeto(item.id)
+
+            return toRow(item, myButtons)
         }))
         .then(rows => rows.forEach(row => {
             tBody.append(row)
@@ -78,27 +86,82 @@ $('.nav-link').toArray().forEach(link => {
     }
 })
 
-const botoesAlunos = `<td><div class="btn-acoes d-none"><button class="btn-adicionar"><i class="fa fa-plus"></i></button><button class="btn-remover"><i class="fa fa-close"></i></button></div></td>`
-
 const getBotoesProjeto = (projeto) => {
-    const td = $('<td>')
-    const acoes = $('<div>').addClass('btn-acoes')
-    const btnAdicionar = $('<button>').addClass('btn-adicionar').html('<i class="fa fa-plus"></i> Alunos')
-    
-    acoes.append(btnAdicionar)
-    td.append(acoes)
+    const td = $('<td>').html('<div class="btn-acoes"> <button class="btn-adicionar"> <i class="fa fa-plus"> </i> Alunos</button> <button class="btn-lista">Lista</button> </div>')
 
+    const btnAdicionar = $(td).find('.btn-adicionar')
+    const btnLista = $(td).find('.btn-lista')
+    
     btnAdicionar.on('click', event => {
         event.preventDefault()
-        getPage('/alunos')
+
+        $.get('./pages/adicionar-alunos-lista.html')
+            .then(response => $('<div>').append(response))
+            .then(page => {
+                const table = $(page).find('.table').get(0)
+
+                table.setAttribute('id-projeto', projeto)
+                $(page).find('.list-title').html('Adicionar Alunos ao Projeto')
+                loadData(table)
+                return page
+            }).then(page => $('#destino').html(page))
+
+    })
+
+    btnLista.on('click', event => {
+        event.preventDefault()
+
+        $.get('./pages/alunos-lista.html')
+            .then(response => $('<div>').append(response))
+            .then(page => {
+                const table = $(page).find('.table').get(0)
+
+                table.setAttribute('id-projeto', projeto)
+                $(page).find('.list-title').html('Lista de Alunos do Projeto')
+
+                const tBody = $(table).find('tbody')
+                tBody.html('')
+            
+                $.get(`/projetos/${projeto}/alunos`)
+                    .then(items => items.map(item => {
+                        return toRow(item)
+                    }))
+                    .then(rows => rows.forEach(row => {
+                        tBody.append(row)
+                    }))
+
+                return page
+            }).then(page => $('#destino').html(page))
+
     })
 
     return td
 }
 
-const getBotoesAluno = (aluno) => {
-    const td = $('<td>')
-    td.append(botoesAlunos)
+const getBotoesAluno = (idAluno) => {
+    const td = $('<td>').html('<td><div class="btn-acoes"><button class="btn-adicionar"><i class="fa fa-plus"></i></button><button class="btn-remover d-none"><i class="fa fa-close"></i></button></div></td>')
+
+    const btnAdicionar = $(td).find('.btn-adicionar')
+    const btnRemover = $(td).find('.btn-remover')
+    btnAdicionar.on('click', () => {
+        const idProjeto = $('.table').get(0).getAttribute('id-projeto')
+
+        $.post(`/projetos/${idProjeto}/alunos`, {idAluno})
+
+        alert('Aluno Adicionado')
+        btnAdicionar.addClass('d-none')
+        btnRemover.removeClass('d-none')
+
+    })
+    
+    btnRemover.on('click', () => {
+        const idProjeto = $('.table').get(0).getAttribute('id-projeto')
+        $.ajax({method: 'delete', url: `/projetos/${idProjeto}/alunos`, data: {idAluno}})
+
+        btnRemover.addClass('d-none')
+        btnAdicionar.removeClass('d-none')
+        alert('Aluno Removido')
+    })
 
     return td
 }
